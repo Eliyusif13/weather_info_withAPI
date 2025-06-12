6122aad035a67761a0ec93976b7d280f2d7003f8
Eliyusif13 <alisadiqov20002@gmail.com>
2025-06-12 16:32:01 +0400

    first commit

---
+++ src/main/java/com/sadiqov/weatherforecast/WeatherForecastApplication.java
@@ -0,0 +1,15 @@
+package com.sadiqov.weatherforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WeatherForecastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForecastApplication.class, args);
+    }
+
+}
